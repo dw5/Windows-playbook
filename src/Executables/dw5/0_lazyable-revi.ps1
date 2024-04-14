96eaf88ffcf450620bc8d2191550df54291b91eb
dw5 <29355584+dw5@users.noreply.github.com>
2024-04-14 20:17:34 +0300

    .

---
+++ src/Executables/dw5/0_lazyable-revi.ps1
@@ -0,0 +1,9 @@
+reg.exe add "HKCU\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" /f /ve
+
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
+$valueName = "EditionSubVersion"
+$valueType = "String"
+$valueData = "ReviOS"
+
+# Set the registry value
+New-ItemProperty -Path $registryPath -Name $valueName -Value $valueData -PropertyType $valueType -Force
