ffcfb2bfc05c7b7b1a027b3295d3590fbf0b88da
dw5 <29355584+dw5@users.noreply.github.com>
2024-04-14 20:54:59 +0300

    .

--- src/Executables/dw5/install-minimal.ps1
+++ src/Executables/dw5/install-minimal.ps1
@@ -3,5 +3,4 @@ Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 scoop install git
 scoop update
 scoop bucket add extras
-scoop install notepadplusplus wget curl
-scoop install crystaldiskinfo
+scoop install notepadplusplus wget curl vlc crystaldiskinfo
